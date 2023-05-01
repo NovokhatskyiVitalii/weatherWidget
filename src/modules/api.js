1cdffb1efaee819f8860840735c54679d86a4e4b
Vitalii <tomrain78@gmail.com>
2023-05-01 15:35:54 +0300

    add search api integration

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,30 +1,28 @@
-const api = () => {
+const apiKey = 'df7ac5c99d7c7f788e64ec68ba2508a3';
 
-    const apiKey = "df7ac5c99d7c7f788e64ec68ba2508a3";
-
-    const fetchData = function (URL, callback) {
-        fetch(`${URL}&appid=${apiKey}`)
-            .then(res => res.json())
-            .then(data => callback(data));
-    }
-
-    const url = {
-        currentWeather(lat, lon) {
-            return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
-        },
-        forecast(lat, lon) {
-            return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
-        },
-        airPollution(lat, lon) {
-            return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
-        },
-        reverseGeo(lat, lon) {
-            return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lot}&limit=5`
-        },
-        geo(query) {
-            return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
-        }
-    }
+const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${apiKey}`)
+        .then((res) => res.json())
+        .then((data) => callback(data));
 };
 
-export default api;
+const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lot}&limit=5`;
+    },
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    },
+};
+
+export { fetchData };
+export { url };
