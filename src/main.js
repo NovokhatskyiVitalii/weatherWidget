3ec7d282fd110b2b4a3c1431270d2bbba065ecd2
Vitalii <tomrain78@gmail.com>
2023-05-04 10:04:24 +0300

    current weather api integration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import './scss/style.scss';
+import "./scss/style.scss";
 
-import app from './modules/app';
+import app from "./modules/app";
+import api from "./modules/api";
+import route from "./modules/route";
+import module from "./modules/module";
 
-window.addEventListener('DOMContentLoaded', () => {
-    app();
-});
+// window.addEventListener("DOMContentLoaded", () => {
+//   app();
+// });
--- src/modules/route.js
+++ src/modules/route.js
@@ -1,48 +1,43 @@
-import {
-    updateWeather,
-    error404
-} from "./app";
+import { updateWeather, error404 } from "./app";
 
-const route = () => {
-    const defaultLocation = "#/weather?lat=50.450001&lon=30.523333" //Kiev
+const defaultLocation = "#/weather?lat=50.450001&lon=30.523333"; //Kiev
 
-    const currentLocation = function () {
-        window.navigator.geolocation.getCurrentPosition(res => {
-            const {
-                latitude,
-                longitude
-            } = res.coords;
+const currentLocation = function () {
+  window.navigator.geolocation.getCurrentPosition(
+    (res) => {
+      const { latitude, longitude } = res.coords;
 
-            updateWeather(`lat=${latitude}`, `lon=${longitude}`);
-        }, err => {
-            window.location.hash = defaultLocation;
-        });
+      updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    },
+    (err) => {
+      window.location.hash = defaultLocation;
     }
-
-    const searchedLocation = query => updateWeather(...query.split("&"))
-    // updateWeather("lat=50.450001", "lon=30.523333");
-
-    const routes = new Map([
-        ["/current-location", currentLocation],
-        ["/weather", searchedLocation]
-    ]);
-
-    const checkHash = function () {
-        const requestURL = window.location.hash.slice(1);
-
-        const [route, query] = requestURL.includes ? requestURL.split("?") : [requestURL];
-
-        routes.get(route) ? routes.get(route)(query) : error404();
-    }
-
-    window.addEventListener("hashchange", checkHash);
-    window.addEventListener("load", function () {
-        if (!window.location.hash) {
-            window.location.hash = "#/current-location";
-        } else {
-            checkHash();
-        }
-    });
+  );
 };
 
-export default route;
+const searchedLocation = (query) => updateWeather(...query.split("&"));
+// updateWeather("lat=50.450001", "lon=30.523333");
+
+const routes = new Map([
+  ["/current-location", currentLocation],
+  ["/weather", searchedLocation],
+]);
+
+const checkHash = function () {
+  const requestURL = window.location.hash.slice(1);
+
+  const [route, query] = requestURL.includes
+    ? requestURL.split("?")
+    : [requestURL];
+
+  routes.get(route) ? routes.get(route)(query) : error404();
+};
+
+window.addEventListener("hashchange", checkHash);
+window.addEventListener("load", function () {
+  if (!window.location.hash) {
+    window.location.hash = "#/current-location";
+  } else {
+    checkHash();
+  }
+});
